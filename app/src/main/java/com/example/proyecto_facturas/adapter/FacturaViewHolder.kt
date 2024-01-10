8ed82278b7fbde0e3838e0a84204db3add494f09
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-01-10 13:35:50 +0100

    CardView Mejorado: - Se pinta de color rojo en caso de que el texto sea "Pendiente de pago" - Se le añade el "€" al final del precio - Fecha Formateada
    Falta viajar entre vistas y hacer la vista de filtros.

--- app/src/main/java/com/example/proyecto_facturas/adapter/FacturaViewHolder.kt
+++ app/src/main/java/com/example/proyecto_facturas/adapter/FacturaViewHolder.kt
@@ -1,15 +1,21 @@
 package com.example.proyecto_facturas.adapter
 
+import android.annotation.SuppressLint
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyecto_facturas.R
 import com.example.proyecto_facturas.databinding.ListaFacturasBinding
 import com.example.proyecto_facturas.domain.Factura
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class FacturaViewHolder(view: View):RecyclerView.ViewHolder(view){ //TODO hacer con retrofit
     val binding = ListaFacturasBinding.bind(view)
     private lateinit var factura: Factura
+
+    @SuppressLint("SetTextI18n")
     fun render(item: Factura, onClickListener:(Factura) -> Unit){
         binding.tvEstadoFactura.text = item.descEstado
         if (item.descEstado=="Pendiente de pago"){
@@ -18,12 +24,26 @@ class FacturaViewHolder(view: View):RecyclerView.ViewHolder(view){ //TODO hacer 
         }else{
             binding.tvEstadoFactura.text = ""
         }
-        binding.tvFechaFactura.text = item.fecha
+        binding.tvFechaFactura.text = formatearFecha(item.fecha)
         binding.tvPrecioFactura.text= item.importeOrdenacion.toString()  + " €"
         itemView.setOnClickListener{
             onClickListener(item)
         }
         factura = item
+    }
+
+    private fun formatearFecha(fechaString: String): String {
+        try {
+            val formatoEntrada = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val fecha = formatoEntrada.parse(fechaString)
+
+            // Formateo la fecha a "dd MMM yyyy"
+            val formatoSalida = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+            return formatoSalida.format(fecha!!)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            return fechaString // Devuelvo la fecha original en caso de error
+        }
     }
 }
 
